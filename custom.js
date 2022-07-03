a75eae05dc2e320b81dea12f5f19bf9530fc9932
Salmalisa <salmalisa7342@gmail.com>
2022-07-03 16:20:15 +0600

    PSD to HTML (Cuda)

---
+++ custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.portfolio nav ul li:nth-child(1)').click(function(){
+    $('.portfolio nav ul li:nth-child(1)').css('background','#fff')
+    return false;
+  });
+
+});
